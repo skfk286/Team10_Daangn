70063dc2ba1769e587c31a9d7157738f0f26162c
민성훈 <gortmtjdgns0943@gmail.com>
2024-08-05 11:22:22 +0900

    UserDTO 작성

--- src/com/devcourse/daangn/entity/UserDTO.java
+++ src/com/devcourse/daangn/entity/UserDTO.java
@@ -1,4 +1,31 @@
 package com.devcourse.daangn.entity;
 
-public class UserDTO {
+public class UserDTO extends BaseDTO{
+    private String user_nickname;
+    private String location;
+
+    public String getUser_nickname() {
+        return user_nickname;
+    }
+
+    public void setUser_nickname(String user_nickname) {
+        this.user_nickname = user_nickname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String toString(){
+        return "UserDTO {" +
+                "user_nickname=" + user_nickname +
+                ", location=" + location +
+                ", createdAt=" + getCreatedAt() +
+                ", updatedAt=" + getUpdatedAt() +
+                "}";
+    }
 }
