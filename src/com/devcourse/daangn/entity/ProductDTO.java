2add30eca5cacd16e26dbac8df18cd19d7922683
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-08-05 11:38:42 +0900

    ProductDTO

--- src/com/devcourse/daangn/entity/ProductDTO.java
+++ src/com/devcourse/daangn/entity/ProductDTO.java
@@ -1,18 +1,62 @@
 package com.devcourse.daangn.entity;
 
-public class ProductDTO extends BaseDTO{
+public class ProductDTO extends BaseDTO {
 
+    private int productId;
+    private int userId;
+    private String title;
+    private String location;
+    private String content;
 
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 
     public String toString() {
-        return "Ingredient{" +
-                "ingredientId=" + ingredientId +
-                ", ingredientName='" + ingredientName + '\'' +
-                ", tan=" + tan +
-                ", dan=" + dan +
-                ", gi=" + gi +
-                ", dang=" + dang +
-                ", calories=" + calories +
+        return "Product{" +
+                "productId=" + productId +
+                ", userId='" + userId +
+                ", title=" + title +
+                ", location=" + location +
+                ", content=" + content +
+                ", createdAt=" + getCreatedAt() +
+                ", updatedAt=" + getUpdatedAt() +
                 '}';
     }
 }
