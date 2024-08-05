f5c95799893be5c8ed5a7b42f8ef9c6dad0f1d84
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-05 11:42:43 +0900

    feat: likeDTO 작성

--- src/com/devcourse/daangn/entity/LikeDTO.java
+++ src/com/devcourse/daangn/entity/LikeDTO.java
@@ -1,4 +1,40 @@
 package com.devcourse.daangn.entity;
 
 public class LikeDTO {
+    private int likeId;
+    private int userId;
+    private int productId;
+
+    public int getLikeId() {
+        return likeId;
+    }
+
+    public void setLikeId(int likeId) {
+        this.likeId = likeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    @Override
+    public String toString() {
+        return "LikeDTO{" +
+                "likeId=" + likeId +
+                ", userId=" + userId +
+                ", productId=" + productId +
+                '}';
+    }
 }
