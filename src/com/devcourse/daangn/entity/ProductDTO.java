09cd215cb119974e81330ce28542752cf24786f9
7zrv <4014133@gmail.com>
2024-08-05 10:22:52 +0900

    feat: baseDTO 추가

--- src/com/devcourse/daangn/entity/ProductDTO.java
+++ src/com/devcourse/daangn/entity/ProductDTO.java
@@ -1,4 +1,18 @@
 package com.devcourse.daangn.entity;
 
-public class ProductDTO {
+public class ProductDTO extends BaseDTO{
+
+
+
+    public String toString() {
+        return "Ingredient{" +
+                "ingredientId=" + ingredientId +
+                ", ingredientName='" + ingredientName + '\'' +
+                ", tan=" + tan +
+                ", dan=" + dan +
+                ", gi=" + gi +
+                ", dang=" + dang +
+                ", calories=" + calories +
+                '}';
+    }
 }
